ca0909dbc65ce339374740ffd553180cc2d24148
VasylTehlivets <vasyltehlivets@gmail.com>
2023-02-28 21:18:03 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home/Home";
+import { MovieDetails } from "./MovieDetails/MovieDetails";
+import { Movies } from "./Movies/Movies";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} >
+          <Route path=":movieId" element={<MovieDetails />} >
+            <Route path=":cast" element={<MovieDetails />} />
+            <Route path=":reviews" element={<MovieDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
   );
 };
